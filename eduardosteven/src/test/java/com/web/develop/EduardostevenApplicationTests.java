af70d087066265e7215f26a5131fc91afb9b4039
LuisRuiz4200 <lr27072001@gmail.com>
2024-03-11 00:49:08 -0500

    despliegue solucionado por la version de compilacion en java y el proyecto

---
+++ eduardosteven/src/test/java/com/web/develop/EduardostevenApplicationTests.java
@@ -0,0 +1,14 @@
+package com.web.develop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class EduardostevenApplicationTests {
+
+    @Test
+    public void contextLoads() {
+        // Puedes realizar tus pruebas aquí
+    }
+
+}
